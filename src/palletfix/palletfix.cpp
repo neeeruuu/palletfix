267f7a5bb8851d1a2ef99a5b7407adb418344377
neru <152752583+neeeruuu@users.noreply.github.com>
2024-06-12 13:16:27 -0300

    feat: automatically clear invalid_mods on first run

--- src/palletfix/palletfix.cpp
+++ src/palletfix/palletfix.cpp
@@ -5,6 +5,55 @@
 #include "util/log.h"
 
 #include <libloaderapi.h>
+#include <shlobj.h>
+#include <comdef.h>
+
+#include <regex>
+#include <string>
+#include <format>
+#include <memory>
+#include <fstream>
+#include <filesystem>
+
+static std::regex modSettingsPattern(R"("mod\.io\.invalid_mods"\s*:\s*\{([^}]*)\})");
+void checkFirstTimeSetup() {
+	PWSTR localLow = NULL;
+
+	if (SHGetKnownFolderPath(FOLDERID_LocalAppDataLow, 0, NULL, &localLow) < 0)
+		return;
+
+	std::shared_ptr<void> llCleanup(localLow, [&](void*) { CoTaskMemFree(localLow); }); // yes i couldn't be arsed to type this for every return
+
+	std::wstring bonelabPath = std::format(L"{}\\Stress Level Zero\\BONELAB", localLow);
+	std::wstring palletFirstTimeCheck = std::format(L"{}\\palfix", bonelabPath);
+
+	if (std::filesystem::exists(palletFirstTimeCheck))
+		return;
+
+	std::wstring modSettingsPath = std::format(L"{}\\mod_settings.json", bonelabPath);
+	std::ifstream modSettingsStream(modSettingsPath);
+
+	if (!modSettingsStream.is_open())
+		return;
+
+	std::string modSettings((std::istreambuf_iterator<char>(modSettingsStream)), std::istreambuf_iterator<char>());
+	modSettingsStream.close();
+	modSettings = std::regex_replace(modSettings, modSettingsPattern, "invalid_mods\":{}");
+
+	std::ofstream modSettingsOut(modSettingsPath);
+	if (!modSettingsOut.is_open())
+		return;
+
+	modSettingsOut << modSettings;
+	modSettingsOut.close();
+
+	std::ofstream firstTimeIdentifier(palletFirstTimeCheck);
+	if (!firstTimeIdentifier.is_open())
+		return;
+	
+	firstTimeIdentifier << "This file exists only so that PalletFix doesn't clear invalid_mods on every load.";
+	firstTimeIdentifier.close();
+}
 
 void PalletFix::load(void* dllMod)
 {
@@ -13,10 +62,14 @@ void PalletFix::load(void* dllMod)
 	Log::setupConsole();
 #endif
 
-	Log::info("init");
+	Log::info("loading");
 
+	checkFirstTimeSetup();
+
+	Log::info("obtaining addresses");
 	interop::getAddresses();
 
+	Log::info("applying hooks");
 	if (!HookManager::get()->applyAll()) {
 		Log::error("failed to apply hooks, unloading...");
 		FreeLibrary(reinterpret_cast<HMODULE>(dllMod));
@@ -26,6 +79,8 @@ void PalletFix::load(void* dllMod)
 
 void PalletFix::unload()
 {
+	Log::info("unloading");
+
 	if (!HookManager::get()->removeAll())
 		Log::warning("failed to remove all hooks");
 }
