0ba313a8889cf170ac41ea5bfea5000bc780e26b
neru <152752583+neeeruuu@users.noreply.github.com>
2024-06-14 04:50:49 -0300

    fix: run load on new thread to allow yielding

--- src/palletfix/dllmain.cpp
+++ src/palletfix/dllmain.cpp
@@ -1,5 +1,7 @@
 #include "palletfix.h"
 
+#include <processthreadsapi.h>
+
 #include "externs.h"
 
 // from: winnt.h
@@ -10,7 +12,8 @@ int DllMain(void* hMod, unsigned long dwReason, void* /*lpReserved*/) {
 	switch (dwReason)
 	{
 	case DLL_PROCESS_ATTACH:
-		PalletFix::load(hMod);
+		CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(PalletFix::load), hMod, 0, 0);
+		//PalletFix::load(hMod);
 		break;
 	case DLL_PROCESS_DETACH:
 		PalletFix::unload();
